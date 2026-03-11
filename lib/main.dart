e1331002f472c72016d8391057bb21516a9393fe
takazatsushiina <138080212+takazatsushiina@users.noreply.github.com>
2026-03-11 16:52:33 +0700

    Refactor HomePage to MainPage with UI enhancements
    Refactor HomePage to MainPage with UI updates including color changes, layout adjustments, and additional widgets for better presentation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,208 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(MainPage());
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.purple,
-          leading: const Icon(Icons.home),
-          title: const Text('First App'),
-        ),
+          backgroundColor: Colors.orange,
+          title: Text('Toko online',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        )),
         body: SingleChildScrollView(
           child: Container(
-            margin: const EdgeInsets.all(20),
+            margin: EdgeInsets.only(top: 20),
             child: Column(
               children: [
-                Row(
-                  children: [const Icon(Icons.archive), const Text('Artikel')],
-                ),
-                Card(
-                  child: Column(
-                    children: [
-                      Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                'Resident Evil Requiem Siap Hadir Tahun 2026',
-                                style: TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-
-                              Image.network(
-                                'https://i.ytimg.com/vi/POz1-EmLsTY/maxresdefault.jpg',
-                              ),
-
-                              const SizedBox(height: 10),
-
-                              const Text(
-                                'Industri game horor kembali diramaikan dengan hadirnya Resident Evil Requiem, judul terbaru dari seri Resident Evil yang dikembangkan oleh Capcom. Game ini merupakan entri utama kesembilan dalam franchise Resident Evil dan dijadwalkan rilis pada 27 Februari 2026 untuk berbagai platform seperti PlayStation 5, Xbox Series X/S, dan PC. Kehadirannya sangat dinantikan oleh para penggemar karena menawarkan cerita baru, teknologi grafis yang lebih maju, serta gameplay yang memadukan horor klasik dengan aksi modern. Seri Resident Evil sendiri telah menjadi salah satu franchise survival horror paling berpengaruh dalam sejarah video game. Dengan setiap seri barunya, Capcom terus berusaha menghadirkan inovasi baik dari segi cerita, gameplay, maupun teknologi grafis. Resident Evil Requiem menjadi bukti upaya tersebut dengan menghadirkan pengalaman bermain yang lebih imersif dan atmosfer horor yang lebih mendalam dibandingkan seri sebelumnya.',
-                                style: TextStyle(fontSize: 14),
-                                textAlign: TextAlign.justify,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Row(
+                Flex(
+                  direction:  Axis.horizontal,
                   children: [
-                    const Text(
-                      'Komentar',
+                    Expanded(
+                      flex: 1,
+                      child: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage('https://picsum.photos/id/237/200/300'),
+                      ),
+                   ),
+                   Expanded(
+                    flex: 10,
+                    child: Text(
+                      "Hai Raju, Selamat datang di toko online kami",
                       style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 16,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
+                   )
                   ],
                 ),
-                ListView(
+                SizedBox(height: 20,),
+                Stack(
+                  children: [
+                    Image.network('https://picsum.photos/seed/picsum/400/200',
+                    fit: .cover,
+                    width: .infinity,
+                    colorBlendMode: .darken,),
+                    Positioned(
+                      bottom: 16,
+                      left: 16,
+                      child: Text('Flash Sale Up to 50%',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: .spaceEvenly,
+                  children: [
+                    TextButton(
+                    onPressed: (){}, 
+                    style: ButtonStyle(
+                      backgroundColor: .all(Colors.orangeAccent)
+                      ),
+                    child: Text('Baju',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    ),
+                    ),
+                    ),
+                    TextButton(
+                    onPressed: (){}, 
+                    style: ButtonStyle(
+                      backgroundColor: .all(Colors.orangeAccent)
+                      ),
+                    child: Text('Celana',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    ),
+                    ),
+                    ),
+                    TextButton(
+                    onPressed: (){}, 
+                    style: ButtonStyle(
+                      backgroundColor: .all(Colors.orangeAccent)
+                      ),
+                    child: Text('Sepatu',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    ),
+                    ),
+                    ),
+                    TextButton(
+                    onPressed: (){}, 
+                    style: ButtonStyle(
+                      backgroundColor: .all(Colors.orangeAccent)
+                      ),
+                    child: Text('Aksesoris',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    ),
+                    ),
+                    ),
+                    TextButton(
+                    onPressed: (){}, 
+                    style: ButtonStyle(
+                      backgroundColor: .all(Colors.orangeAccent)
+                      ),
+                    child: Text('Elektronik',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    ),
+                    ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20,),
+                GridView.count(
+                  crossAxisCount:2,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 10,
                   shrinkWrap: true,
                   children: [
                     Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(3),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Ryzz'),
-                            Text(
-                              'Asli sih keren banget gamenya!! gak sabar pengen main!!.',
-                            ),
-                          ],
-                        ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network('https://images.unsplash.com/1/work-station-straight-on-view.jpg?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/400/200',
+                          fit: BoxFit.cover,
+                          width: .infinity,
+                          height: 200,),
+                          Text('Macbook Pro'),
+                          Text('Rp. 10.000.000',
+                          style: TextStyle(
+                            color: Colors.orange,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
                       ),
                     ),
                     Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(3),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Leonardo, paggil aja nardo'),
-                            Text(
-                              'Pastinya ini game bakal booming banget, dah kaya gow ragnarok pokoknya.',
-                            ),
-                          ],
-                        ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network('https://images.unsplash.com/photo-1622123249577-9f81e9a94a27?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDM5fHx8ZW58MHx8fHx8',
+                          fit: BoxFit.cover,
+                          width: .infinity,
+                          height: 200,),
+                          Text('Macbook Air Pro'),
+                          Text('Rp. 11.000.000',
+                          style: TextStyle(
+                            color: Colors.orange,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+                    ),
+                    Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network('https://d29c1z66frfv6c.cloudfront.net/pub/media/catalog/product/zoom/1d42d64e49f91af45d16c442eea2065e6eab9c07_xxl-1.jpg',
+                          fit: BoxFit.cover,
+                          width: .infinity,
+                          height: 200,),
+                          Text('Kemeja Lengan Pendek'),
+                          Text('Rp. 200.000',
+                          style: TextStyle(
+                            color: Colors.orange,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+                    ),Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSriYliYb4mQHjjCvyEKmGDLqrym-ctjstNRQ&s',
+                          fit: BoxFit.cover,
+                          width: .infinity,
+                          height: 200,),
+                          Text('Kaos Oblong'),
+                          Text('Rp. 100.000',
+                          style: TextStyle(
+                            color: Colors.orange,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
                       ),
                     ),
                   ],
@@ -109,7 +211,7 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ),
-      ),
+    ),
     );
   }
 }
